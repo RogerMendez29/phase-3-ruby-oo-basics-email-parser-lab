383362bb9c9f8db6ffd13258c200dad29fee8e9c
Roger Mendez <roger61087@gmail.com>
2021-12-20 12:36:21 -0500

    commit tree 038f4962753cbf6bf99aaad27ab2eafebfe389df

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,32 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+
+require 'pry'
+
+class EmailAddressParser
+    def initialize(email_list)
+        @email_list = email_list
+    end
+
+    def parse
+        update= @email_list.gsub(/[,]/,"")
+        email_array = update.split(" ")
+
+        delete_duplicates = email_array.uniq
+    #    binding.pry
+
+    delete_duplicates
+
+
+       
+        
+    end
+    
+end
+
+email_addresses = "john@doe.com, person@somewhere.org, roger@gmail.com, angel@gmail.com lucas@gmail.com person@somewhere.org, person@somewhere.org"
+parser = EmailAddressParser.new(email_addresses)
+
+parser.parse
